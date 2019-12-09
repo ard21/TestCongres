a121a620e24b052551bdc494050bfbce16e70d7e
Alain Robillard <arobillard@gestionquattra.onmicrosoft.com>
2019-12-09 11:25:49 -0500

    test du 9

--- TestCongres/TestCongres/Atelier.xaml.cs
+++ TestCongres/TestCongres/Atelier.xaml.cs
@@ -17,18 +17,27 @@ namespace TestCongres
         public Atelier()
         {
             InitializeComponent();
-            var htmlSource = new HtmlWebViewSource();
-            htmlSource.Html = @"<html><body>
-                <h1>SEVGI KELCI<br>
-                    Notaire<br> 
-                    ANTOINE GUILMAIN<br>
-                    Avocat, docteur en droit<br>
-                    FASKEN </h1>
-                <p>La protection et la sécurité de l'information personnelle ou confidentielle de ses clients sont particulièrement préoccupantes pour le notaire d'aujourd'hui. Un accès non autorisé ou une perte des données de clients découlant d'une atteinte aux mesures de sécurité peut mettre en péril le devoir de secret professionnel incombant à tout notaire, nuire à la bonne réputation de la profession et à son aptitude à servir l'intérêt public, tout en causant des préjudices graves aux clients.</p>
-                <p>Dans ce contexte, le notaire doit assurer la sécurité des renseignements de ses clients, une obligation reconnue aujourd'hui, tout en évaluant s'il doit déclarer dans certaines circonstances la survenance d'un incident de sécurité aux clients et autorités concernés, une pratique méconnue pour le moment.</p>
-                <p>Les conférenciers font la lumière sur les obligations du notaire en matière de protection des renseignements personnels, et le conseillent sur les bonnes pratiques à instaurer pour une utilisation responsable des technologies de l'information dans l'exercice de sa profession.</p>
-              </body></html>";
-            leTexte.Source = htmlSource;
+            //var htmlSource = new HtmlWebViewSource();
+            //htmlSource.Html = @"<html><body>
+            //    <h1>SEVGI KELCI<br>
+            //        Notaire<br> 
+            //        ANTOINE GUILMAIN<br>
+            //        Avocat, docteur en droit<br>
+            //        FASKEN </h1>
+            //    <p>La protection et la sécurité de l'information personnelle ou confidentielle de ses clients sont particulièrement préoccupantes pour le notaire d'aujourd'hui. Un accès non autorisé ou une perte des données de clients découlant d'une atteinte aux mesures de sécurité peut mettre en péril le devoir de secret professionnel incombant à tout notaire, nuire à la bonne réputation de la profession et à son aptitude à servir l'intérêt public, tout en causant des préjudices graves aux clients.</p>
+            //    <p>Dans ce contexte, le notaire doit assurer la sécurité des renseignements de ses clients, une obligation reconnue aujourd'hui, tout en évaluant s'il doit déclarer dans certaines circonstances la survenance d'un incident de sécurité aux clients et autorités concernés, une pratique méconnue pour le moment.</p>
+            //    <p>Les conférenciers font la lumière sur les obligations du notaire en matière de protection des renseignements personnels, et le conseillent sur les bonnes pratiques à instaurer pour une utilisation responsable des technologies de l'information dans l'exercice de sa profession.</p>
+            //  </body></html>";
+            //leTexte.Source = htmlSource;
+
+            //var browser = new WebView();
+            //var htmlSource = new HtmlWebViewSource();
+            //htmlSource.Html = @"<html><body>
+            //    <h1>Xamarin.Forms</h1>
+            //    <p>Welcome to WebView.</p>
+            //    </body></html>";
+            //browser.Source = htmlSource;
+            //leTexte.Source = htmlSource;
         }
 
         async private void btnBack(object sender, EventArgs e)
