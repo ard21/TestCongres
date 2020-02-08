6e42d0007a2d328610f4917b05de76bea819226b
Alain Robillard <arobillard@gestionquattra.onmicrosoft.com>
2020-02-08 12:06:38 -0500

    Favori avant lien

--- TestCongres/TestCongres/Favoris.xaml.cs
+++ TestCongres/TestCongres/Favoris.xaml.cs
@@ -14,52 +14,90 @@ namespace TestCongres
 
     public partial class Favoris : ContentPage
     {
-		public IList<GroupedFavorisModel> grouped { get; private set; }
+		//public IList<GroupedFavorisModel> grouped { get; private set; }
+		public List<GroupedFavorisModel> favori { get; private set; } = new List<GroupedFavorisModel>();
 		public Favoris()
         {
             InitializeComponent();
 
-			grouped = new List<GroupedFavorisModel>();
-			var conferencierGroup = new GroupedFavorisModel() { LongName = "Conférenciers", ShortName = "c" };
-			var exposantsGroup = new GroupedFavorisModel() { LongName = "Exposants", ShortName = "e" };
-			var sessionsGroup = new GroupedFavorisModel() { LongName = "Sessions", ShortName = "s" };
-			conferencierGroup.Add(new FavorisModel() { Nom = "Johanne Lambert", Comment = "Rédaction des fiducies de gel", ImageURL = "Carolle DALLAIRE.png" });
-			conferencierGroup.Add(new FavorisModel() { Nom = "Marc Tremblay", Comment = "Assurance de personnes", ImageURL = "André PLANTE.png" });
-			exposantsGroup.Add(new FavorisModel() { Nom = "Atleon inc.", Comment = "Technologie de votation" });
-			exposantsGroup.Add(new FavorisModel() { Nom = "Falcan Medical", Comment = "Lits et fauteuils" });
-			exposantsGroup.Add(new FavorisModel() { Nom = "MTM - Medical Tronik", Comment = "Matériel médical de haute technologie" });
-			sessionsGroup.Add(new FavorisModel() { Nom = "Les dix recommandations du syndic", Comment = "Local 210E, 8 h" });
-			sessionsGroup.Add(new FavorisModel() { Nom = "Protection et sécurité de l'information", Comment = "local 510, 9 h 30" });
+			favori.Add(new GroupedFavorisModel("Conférenciers", new List<FavorisModel>
+				{
+					new FavorisModel
+					{
+						Nom = "Johanne Lambert", 
+						Comment = "Rédaction des fiducies de gel", 
+						ImageURL = "Carolle DALLAIRE.png"
+					},
+					new FavorisModel
+					{
+						Nom = "Marc Tremblay", 
+						Comment = "Assurance de personnes", 
+						ImageURL = "André PLANTE.png"                   },
+				}));
 
-			grouped.Add(conferencierGroup);
-			grouped.Add(exposantsGroup);
-			grouped.Add(sessionsGroup);
+			favori.Add(new GroupedFavorisModel("Exposants", new List<FavorisModel>
+				{
+					new FavorisModel
+					{
+						Nom = "Atleon inc.", 
+						Comment = "Technologie de votation",
+						ImageURL = "LogoAtleonSeul@2x.png"
+					},
+					new FavorisModel
+					{
+						Nom = "Falcan Medical", 
+						Comment = "Lits et fauteuils",
+						ImageURL = "falcan.gif"
+					},
+					new FavorisModel
+					{
+						Nom = "MTM - Medical Tronik", 
+						Comment = "Matériel médical de haute technologie",
+						ImageURL = "mtm.png"
+				}}));
+
+			favori.Add(new GroupedFavorisModel("Sessions", new List<FavorisModel>
+				{
+					new FavorisModel
+					{
+						Nom = "Les dix recommandations du syndic", 
+						Comment = "Local 210E, 8 h"
+					},
+					new FavorisModel
+					{
+						Nom = "Protection et sécurité de l'information", 
+						Comment = "local 510, 9 h 30"
+				}}));
+
+			//var exposantsGroup = new GroupedFavorisModel() { LongName = "Exposants", ShortName = "e" };
+			//var sessionsGroup = new GroupedFavorisModel() { LongName = "Sessions", ShortName = "s" };
+			//exposantsGroup.Add(new FavorisModel() { Nom = "Atleon inc.", Comment = "Technologie de votation" });
+			//exposantsGroup.Add(new FavorisModel() { Nom = "Falcan Medical", Comment = "Lits et fauteuils" });
+			//exposantsGroup.Add(new FavorisModel() { Nom = "MTM - Medical Tronik", Comment = "Matériel médical de haute technologie" });
+			//sessionsGroup.Add(new FavorisModel() { Nom = "Les dix recommandations du syndic", Comment = "Local 210E, 8 h" });
+			//sessionsGroup.Add(new FavorisModel() { Nom = "Protection et sécurité de l'information", Comment = "local 510, 9 h 30" });
+
+			//grouped.Add(conferencierGroup);
+			//grouped.Add(exposantsGroup);
+			//grouped.Add(sessionsGroup);
 
 			BindingContext = this;
 
-			//lstView.ItemsSource = grouped;
-			//lstView.IsGroupingEnabled = true;
-			//lstView.GroupDisplayBinding = new Binding("LongName");
-			//lstView.GroupShortNameBinding = new Binding("ShortName");
+			//lvFavoris.ItemsSource = grouped;
+			//lvFavoris.IsGroupingEnabled = true;
+			//lvFavoris.GroupDisplayBinding = new Binding("LongName");
+			//lvFavoris.GroupShortNameBinding = new Binding("ShortName");
 
-			//lstView.ItemTemplate = new DataTemplate(typeof(TextCell));
-			//lstView.ItemTemplate.SetBinding(TextCell.TextProperty, "Nom");
-			//			lstView.ItemTemplate.SetBinding (TextCell.DetailProperty, "Comment");
+			//lvFavoris.ItemTemplate = new DataTemplate(typeof(ViewCell));
+			//lvFavoris.ItemTemplate.SetBinding(TextCell.TextProperty, "Nom");
+			//lvFavoris.ItemTemplate.SetBinding (TextCell.DetailProperty, "Comment");
 
-			//Content = lstView;
+			//Content = lvFavoris;
 
 		}
-		async void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+		async private void OnCollectionViewSelectionChanged(object sender, SelectionChangedEventArgs e)
 		{
-			Fichier selectedItem = e.SelectedItem as Fichier;
 			await Navigation.PushModalAsync(new MessagesDetail(), false);
-
 		}
-
-		void OnListViewItemTapped(object sender, ItemTappedEventArgs e)
-		{
-			Fichier tappedItem = e.Item as Fichier;
-		}
-
 	}
 }
--- TestCongres/TestCongres/FavorisModel.cs
+++ TestCongres/TestCongres/FavorisModel.cs
@@ -11,14 +11,14 @@ namespace TestCongres
 		public string Nom { get; set; }
 		public string Comment { get; set; }
 		public string ImageURL { get; set; }
-		public FavorisModel()
-		{
-		}
-
 	}
-	public class GroupedFavorisModel : ObservableCollection<FavorisModel>
+	public class GroupedFavorisModel : List<FavorisModel>
 	{
 		public string LongName { get; set; }
 		public string ShortName { get; set; }
+		public GroupedFavorisModel(string name, List<FavorisModel> favori) : base(favori)
+		{
+			LongName = name;
+		}
 	}
 }
