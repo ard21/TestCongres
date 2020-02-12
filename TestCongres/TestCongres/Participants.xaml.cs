167cc1367ba2c894d6002456ad8abe506b5d876b
Alain Robillard <arobillard@gestionquattra.onmicrosoft.com>
2020-02-12 11:07:18 -0500

    lien examen

--- TestCongres/TestCongres/Participants.xaml.cs
+++ TestCongres/TestCongres/Participants.xaml.cs
@@ -11,6 +11,7 @@ namespace TestCongres
         public Participants()
         {
             InitializeComponent();
+            webViewVote.Source = "https://secure.innovision.qc.ca/demo/Membres/login.aspx?param1=2125&param2=827CCB0EEA8A706C4C34A16891F84E7B";
         }
         async private void btnBack(object sender, EventArgs e)
         {
